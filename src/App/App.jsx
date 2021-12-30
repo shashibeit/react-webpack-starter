251eb36ccf6d0b8d08e9ab5cb1f18a189d0233b7
shashibeit <shashibeit@gmail.com>
2021-12-30 22:58:33 +0530

    Initial Release

---
+++ src/App/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import './style.scss'
+function App() {
+  return (
+
+    <div className="container">
+
+      <div className="jumbotron">
+        <h1 className="align-center">React Webpack Starter</h1>  
+        <hr  className="bg-success border-4 border-top border-success"/>       
+          <p>A boiler plate for creating react applications bundled by webpack (using ES6+, Babel, SASS and webpack development server)</p>
+      </div>    
+    </div>
+  );
+}
+
+export { App };
---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    resolve: {
+        extensions: ['.js', '.jsx']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                loader: 'babel-loader'
+            },
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                  // Creates `style` nodes from JS strings
+                  "style-loader",
+                  // Translates CSS into CommonJS
+                  "css-loader",
+                  // Compiles Sass to CSS
+                  "sass-loader",
+                ],
+              },
+        ]
+    },
+    plugins: [new HtmlWebpackPlugin({
+        template: './public/index.html'
+    })],
+    devServer: {
+        historyApiFallback: true
+    },
+    externals: {
+        // global app config object
+        config: JSON.stringify({
+            apiUrl: 'http://localhost:4000'
+        })
+    }
+}
